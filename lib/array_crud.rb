e3ca509af6fffd742ab907d2d4db5c9f144d4567
Kristian Portillo <portillk@gmail.com>
2020-07-05 21:41:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,23 +7,28 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = [1,2,3]
+  array << 4
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = [1,2,3]
+  array[0] = 0
 end
 
 def remove_element_from_end_of_array(array)
-  
+array = [1,2,3]
+gone = array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = [1,2,3]
+  replace = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = [1,2,3]
+  p array[1]
 end
 
 def retrieve_first_element_from_array(array)
