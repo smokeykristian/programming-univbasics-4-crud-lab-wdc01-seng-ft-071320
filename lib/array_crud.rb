4302ea3a7f9316fb63db15d310d7c2fff1513e0b
Kristian Portillo <portillk@gmail.com>
2020-07-05 21:50:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,13 +8,13 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["hi","yo","wassup"]
-  element = "hello"
+  element = "arrays!"
   array << "hello"
 end
 
 def add_element_to_start_of_array(array, element)
   array = [1,2,3]
-  array.unshift(0) 
+  array.unshift("wow") 
 end
 
 def remove_element_from_end_of_array(array)
