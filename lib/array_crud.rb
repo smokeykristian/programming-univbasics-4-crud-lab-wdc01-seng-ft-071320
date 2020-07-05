fe3df0caa0f1f8b492241e539e4ac81518ae5f2d
Kristian Portillo <portillk@gmail.com>
2020-07-05 21:53:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,12 +18,12 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-array = [1,2,3]
+array = [1,2,"arrays!"]
 gone = array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  array = [1,2,3]
+  array = ["wow",2,3]
   replace = array.shift
 end
 
