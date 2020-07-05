82ad91aca00fb3ecc6dadc5f111d4a0b2b96afa1
Kristian Portillo <portillk@gmail.com>
2020-07-05 21:51:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,9 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["hi","yo","wassup"]
+  array =  ["wow", "I", "am", "really", "learning"]
   element = "arrays!"
-  array << "hello"
+  array << ""
 end
 
 def add_element_to_start_of_array(array, element)
